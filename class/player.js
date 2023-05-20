b39e2ed82aa666b2b1cd163341dbb643a6f195b9
Jimmy Diep <jdiep07@gmail.com>
2023-05-20 10:10:06 -0700

    eatItem issues

--- class/player.js
+++ class/player.js
@@ -1,63 +1,78 @@
-const { Food } = require('./food');
+const { Food } = require("./food")
 
 class Player {
+  constructor(name, startingRoom) {
+    this.name = name
+    this.currentRoom = startingRoom
+    this.items = []
+  }
 
-    constructor(name, startingRoom) {
-        this.name = name;
-        this.currentRoom = startingRoom;
-        this.items = [];
+  move(direction) {
+    const nextRoom = this.currentRoom.getRoomInDirection(direction)
+
+    // If the next room is valid, set the player to be in that room
+    if (nextRoom) {
+      this.currentRoom = nextRoom
+
+      nextRoom.printRoom(this)
+    } else {
+      console.log("You cannot move in that direction")
     }
+  }
 
-    move(direction) {
-
-        const nextRoom = this.currentRoom.getRoomInDirection(direction);
-
-        // If the next room is valid, set the player to be in that room
-        if (nextRoom) {
-            this.currentRoom = nextRoom;
-
-            nextRoom.printRoom(this);
-        } else {
-            console.log("You cannot move in that direction");
-        }
+  printInventory() {
+    if (this.items.length === 0) {
+      console.log(`${this.name} is not carrying anything.`)
+    } else {
+      console.log(`${this.name} is carrying:`)
+      for (let i = 0; i < this.items.length; i++) {
+        console.log(`  ${this.items[i].name}`)
+      }
     }
+  }
 
-    printInventory() {
-        if (this.items.length === 0) {
-            console.log(`${this.name} is not carrying anything.`);
-        } else {
-            console.log(`${this.name} is carrying:`);
-            for (let i = 0 ; i < this.items.length ; i++) {
-                console.log(`  ${this.items[i].name}`);
-            }
-        }
+  takeItem(itemName) {
+    const newItem = this.currentRoom.getItemByName(itemName)
+
+    if (newItem) {
+      const leftovers = this.currentRoom.items.filter(
+        (eachItem) => eachItem !== newItem
+      )
+      this.currentRoom.items = leftovers
+
+      this.items.push(newItem)
     }
+  }
 
-    takeItem(itemName) {
-        // Picks up an item from the current room into the player's inventory
+  dropItem(itemName) {
+    const newItem = this.getItemByName(itemName)
 
-        // Your code here
+    if (newItem) {
+      const leftovers = this.items.filter((eachItem) => eachItem !== newItem)
+      this.items = leftovers
+
+      this.currentRoom.items.push(newItem)
     }
+  }
 
-    dropItem(itemName) {
-        // Drops an item the player is holding into their current room
+  eatItem(itemName) {
+    // Allow the player to eat food items, but not non-food items
 
-        // Your code here
+    const newItem = this.getItemByName(itemName)
+
+    if (!(newItem instanceof Food)) {
+      //   const leftovers = this.items.filter((eachItem) => eachItem !== newItem)
+      //   this.items = leftovers
+      this.items.push(newItem)
     }
+  }
 
-    eatItem(itemName) {
-        // Allow the player to eat food items, but not non-food items
-
-        // Your code here
-    }
-
-    getItemByName(name) {
-        // Retrieves an item from a player's inventory by item name
-
-        // Your code here
-    }
+  getItemByName(name) {
+    const newItem = this.items.find((eachItem) => eachItem.name === name)
+    return newItem
+  }
 }
 
 module.exports = {
   Player,
-};
+}
